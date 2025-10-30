ae4fd55d8794a4596b7e73ccb64b03c42968b25f
François Belleau <francois.belleau@gmail.com>
2025-10-30 00:34:25 -0400

    Update test_pot.py

--- test_pot.py
+++ test_pot.py
@@ -1,17 +1,21 @@
 # test_pot.py
 
-from machine import ADC
-from time import sleep
+from machine import Pin, ADC
+import time
 
-# Initialiser l'objet ADC sur le pin A0 (canal 0 pour l'ESP8266)
-pot = ADC(0) 
+# ESP32
 
+adc_1 = ADC(Pin(15))
+adc_2 = ADC(Pin(4))
+
+print("Démarrage de la lecture du potentiomètre...")
+
+# 3. Boucle de lecture
 while True:
-    # Lire la valeur analogique (généralement entre 0 et 1023 pour l'ESP8266)
-    valeur_pot = pot.read() 
+    # Lire la valeur analogique (un nombre entier entre 0 et 4095)
+    raw_value_1 = adc_1.read()
+    raw_value_2 = adc_2.read()
     
-    # Afficher la valeur
-    print("Valeur du potentiomètre :", valeur_pot) 
+    print(f"P1: {raw_value_1: <4} P2: {raw_value_2: <4}")
     
-    # Attendre un peu avant la prochaine lecture
-    sleep(0.1)
+    time.sleep(0.1) # Lire toutes les 100 millisecondes
